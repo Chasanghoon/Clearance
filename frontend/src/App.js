5d8ca12a373d80c590e3c8f2251adb9e278c8028
김범주 <kbjid17@naver.com>
2022-04-26 12:02:56 +0900

    [S06P31E203-117] Fix: 리액트 개발환경 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import pengu from './pengu.jpg'
 import './App.css';
 
 function App() {
   return (
       <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pengu} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
