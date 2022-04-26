109c091e7413a95b71efa36381b5916e0df268f5
차상훈 <sanghunc62@gmail.com>
2022-04-26 15:35:11 +0900

    [S06P31E203-133] Feat: nginx 설치/설정

---
+++ backend/build/generated/sources/annotationProcessor/java/main/com/ssafy/cleanrance/domain/user/db/entity/QUser.java
@@ -0,0 +1,57 @@
+package com.ssafy.cleanrance.domain.user.db.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = -1350160178L;
+
+    public static final QUser user = new QUser("user");
+
+    public final StringPath userAddress = createString("userAddress");
+
+    public final StringPath userEmail = createString("userEmail");
+
+    public final StringPath userId = createString("userId");
+
+    public final StringPath userImage = createString("userImage");
+
+    public final DateTimePath<java.time.LocalDateTime> userJoindata = createDateTime("userJoindata", java.time.LocalDateTime.class);
+
+    public final StringPath userLicensenum = createString("userLicensenum");
+
+    public final StringPath userPassword = createString("userPassword");
+
+    public final StringPath userPhone = createString("userPhone");
+
+    public final NumberPath<Integer> userRole = createNumber("userRole", Integer.class);
+
+    public final StringPath userStore = createString("userStore");
+
+    public final BooleanPath userStoresignup = createBoolean("userStoresignup");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
