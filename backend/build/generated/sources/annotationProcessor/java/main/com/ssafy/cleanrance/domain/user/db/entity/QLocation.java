e9df9cbe070ffb01cc6f9ed640d3c5c93220e6f0
김윤지 <yoonjigim0.0@gmail.com>
2022-04-27 17:38:00 +0900

    [S06P31E203-150] Feat: 매장 회원가입

---
+++ backend/build/generated/sources/annotationProcessor/java/main/com/ssafy/cleanrance/domain/user/db/entity/QLocation.java
@@ -0,0 +1,57 @@
+package com.ssafy.cleanrance.domain.user.db.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QLocation is a Querydsl query type for Location
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QLocation extends EntityPathBase<Location> {
+
+    private static final long serialVersionUID = -1382110408L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QLocation location = new QLocation("location");
+
+    public final NumberPath<Integer> locationId = createNumber("locationId", Integer.class);
+
+    public final NumberPath<Double> locationXpoint = createNumber("locationXpoint", Double.class);
+
+    public final NumberPath<Double> locationYpoint = createNumber("locationYpoint", Double.class);
+
+    public final QUser user;
+
+    public final StringPath userId = createString("userId");
+
+    public QLocation(String variable) {
+        this(Location.class, forVariable(variable), INITS);
+    }
+
+    public QLocation(Path<? extends Location> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QLocation(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QLocation(PathMetadata metadata, PathInits inits) {
+        this(Location.class, metadata, inits);
+    }
+
+    public QLocation(Class<? extends Location> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user"), inits.get("user")) : null;
+    }
+
+}
+
--- backend/src/main/java/com/ssafy/cleanrance/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/cleanrance/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.ssafy.cleanrance.domain.user.service;
 
 import com.ssafy.cleanrance.domain.user.db.entity.User;
+import com.ssafy.cleanrance.domain.user.request.StoreSignUpRequest;
+import com.ssafy.cleanrance.domain.user.request.UserSignUpRequest;
 
 public interface UserService {
     //회원가입
-    User createUser(User user);
+    String createStore(StoreSignUpRequest storeSignUpRequest);
+    String createUser(UserSignUpRequest userSignUpRequest);
     //회원정보 조회
     User findById(String userId);
     //이메일로 회원정보 조회
@@ -12,5 +15,5 @@ public interface UserService {
     //회원정보 수정
 //    User updateUser();
     //회원 탈퇴
-//    void deleteUser();
+    String deleteUser(String userId);
 }
