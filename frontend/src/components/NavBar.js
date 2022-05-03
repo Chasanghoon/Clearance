58436c61b330a3072b83483473a8b793179e6640
김범주 <kbjid17@naver.com>
2022-05-04 01:26:01 +0900

    [S06P31E203-159] Feat: 카카오맵 현재 위치정보 가져오기 및 상품 리스트 api 코드 제작

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,14 +4,36 @@ import Button from "react-bootstrap/Button";
 
 const NavBar = () => {
 
+    const logout = () => {
+        console.log(sessionStorage.getItem("id"));
+        sessionStorage.clear();
+    }
+    
     return (
-        <div>
-            <span>clearance</span>
+    
+        <div style={{
+            backgroundColor:"beige"
+        }}>
             <span style={{
-                marginLeft:'50%'
+                marginLeft:'1%'
+            }}>clearance</span>
+            <span style={{
+                marginLeft:'40%'
             }}>
-            <Link to="../login"><Button> log in </Button></Link>
-            <Link to="../register"><Button> sign up </Button></Link>
+            <Link to="../login"><Button style={{
+                display: sessionStorage.getItem("id") === null ? "none" : "block",
+            }}> mypage </Button></Link>
+            <Link to = "../"><Button onClick={logout} style={{
+                display: sessionStorage.getItem("id") === null ? "none" : "block",
+            }}> logout </Button></Link>
+            {/* <Link to="../login"><Button style={{
+                display: sessionStorage.getItem("id") === null ? "block" : "none",
+            }}> log in </Button></Link>
+            <Link to="../register"><Button style={{
+                display: sessionStorage.getItem("id") === null ? "block" : "none",
+            }}> sign up </Button></Link> */}
+            
+
             </span>
         </div>
     )
