d3d3cd3fc1d3625515e5e0cdcbaa9e0c9e2a1755
김윤지 <yoonjigim0.0@gmail.com>
2022-05-03 11:14:59 +0900

    [S06P31E203-176] Feat: 사용자 거래내역조회

---
+++ backend/src/main/java/com/ssafy/cleanrance/domain/consumer/mypage/controller/ConsumerController.java
@@ -0,0 +1,50 @@
+package com.ssafy.cleanrance.domain.consumer.mypage.controller;
+
+import com.ssafy.cleanrance.domain.consumer.mypage.db.entity.Book;
+import com.ssafy.cleanrance.domain.consumer.mypage.service.ConsumerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Api(value = "구매자 API", tags = {"Consumer"})
+@RestController
+@RequestMapping("/api")
+public class ConsumerController {
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/book")
+    @ApiOperation(value = "회원 예약 내역 조회", notes = "회원 아이디로 예약정보를 조회후 응답한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<List<Book>> findBookByUser(@RequestParam String userId){
+        List<Book> list = consumerService.findBookByuserId(userId);
+        return ResponseEntity.status(200).body(list);
+    }
+
+    @GetMapping("/book")
+    @ApiOperation(value = "회원의 날짜별 예약 내역 조회", notes = "회원 아이디와 날짜로 예약정보를 조회후 응답한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<List<Book>> findBookByUser(@RequestParam String userId, @RequestParam String date){
+        List<Book> list = consumerService.findBookByuserId(userId);
+        return ResponseEntity.status(200).body(list);
+    }
+}
---
+++ backend/src/main/java/com/ssafy/cleanrance/domain/consumer/mypage/service/ConsumerService.java
@@ -0,0 +1,16 @@
+package com.ssafy.cleanrance.domain.consumer.mypage.service;
+
+import com.ssafy.cleanrance.domain.consumer.mypage.db.entity.Book;
+
+import java.util.List;
+
+public interface ConsumerService {
+    //전체 예약 내역 조회
+    List<Book> findBookByuserId(String userId);
+    //날짜별 예약 내역 조회
+    List<Book> findBookByDate(String userId, String date);
+    //진행 상황에 따른 예약 내역 조회
+    //거래내역 조회
+    //탄소발자국 조회
+    //QR코드 생성
+}
