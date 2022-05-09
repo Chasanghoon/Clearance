512a58a4062507b08a3d93d5881c7ba7e396cedb
김윤지 <yoonjigim0.0@gmail.com>
2022-05-09 17:37:19 +0900

    [S06P31E203-245] Fix: 상품조회 수정

--- backend/src/main/java/com/ssafy/cleanrance/domain/product/service/ProductService.java
+++ backend/src/main/java/com/ssafy/cleanrance/domain/product/service/ProductService.java
@@ -6,6 +6,8 @@ import com.ssafy.cleanrance.domain.product.db.entity.ProductCategory;
 import com.ssafy.cleanrance.domain.product.request.ProductRegisterRequest;
 import com.ssafy.cleanrance.domain.product.request.ProductUpdatePutRequest;
 import com.ssafy.cleanrance.domain.product.response.ProductFindStoreId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,21 +24,16 @@ public interface ProductService {
     // 상품 삭제
     @Transactional
     void removeProduct(Integer productId);
-    // 상품정보 수정
-    //Optional<Product> updateStore(Product product);
     // 매장별 상품 조회
     List<Product> findProductByStoreId(String storeId);
-//    List<ProductFindStoreId> findProductByStoreId(String storeId) throws ParseException;
-    //매장 & 카테고리별 상품 조회
-    List<Product> findProductByStoreIdAndCategory(String storeId, int categoryId);
-    //매장 & 검색어 상품 조회
-    List<Product> findProductByStoreIdAndWord(String storeId, String word);
     //카테고리 목록 조회
     List<ProductCategory> findProductCategory();
-
+    //상품 수정
     Product updateProduct(ProductUpdatePutRequest productUpdatePutRequest);
-
+    //날짜별 상품 조회
     List<Product> findProductByDate(String userId, String date);
-
-    List<Product> findProductList(String storeId, int categoryId, String word);
+    //입력값에 따른 상품 조회
+    List<Product> findProductList(double ypoint, double xpoint, String storeId, int categoryId, String word);
+    //매장별 상품 조회
+    Page<Product> findProductByStoreId(String storeId, Pageable pageable);
 }
