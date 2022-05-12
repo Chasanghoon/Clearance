f15558a1c36e0ded03d72886c64666794dbe78d3
rkdrhkddms <rhkddms3501@naver.com>
2022-05-13 01:21:10 +0900

    [S06P31E203-210] Feat : 마이페이지_유저 전체 예약 내역_전체예약리스트

--- frontend/src/components/myPage/store/product/AllProductManagement.js
+++ frontend/src/components/myPage/store/product/AllProductManagement.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, FormControl, InputGroup, ModalFooter, Table } from 'react-bootstrap';
+import { Button, FormControl, InputGroup, ModalFooter, Pagination, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal'
 import UpdateProduct from './UpdateProduct';
@@ -11,34 +11,50 @@ function AllProductManagement(props) {
     const [deleteCheck, setDelectCheck] = useState(false);
     const [modalShow, setModalShow] = React.useState(false);
 
-    console.log(sessionStorage.getItem("id"));
+    const [word, setWord] = useState("");
+    const [checkWord, setCheckWord] = useState(false);
 
+    const [totalPage, setTotalPage] = useState();
+    const [page, setPage] = useState(1);
+    const [firstPage, setFirstPage] = useState(1);
+    const [lastPage, setLastPage] = useState(5);
+    const size = 6;
+    let items = [];
+    let showItems = [];
+
+
+    console.log(sessionStorage.getItem("id"));
+    console.error(checkWord);
     useEffect(() => {
+        console.log("실행샐행!!!!!!!!!!!!!!")
         // ! axios get
         console.log("axios get")
         axios
-            .get(`http://localhost:8080/api/product/info/?storeId=${sessionStorage.getItem("id")}`)
+            .get(`http://localhost:8080/api/product/info?page=${page}&size=${size}&storeId=${sessionStorage.getItem("id")}&word=${word}`)
             .then((result) => {
-                console.log(result.data.content);
+                console.log(result.data);
+
                 setProduct(result.data.content);
+                setTotalPage(result.data.totalPages);
+                setTotalPage(21);
             })
             .catch((e) => {
                 // console.error("axios get 실패");
                 console.error(e)
             });
-    }, [deleteCheck]);
+    }, [deleteCheck,checkWord, page]);
 
-    function deleteProduct(){
+    function deleteProduct() {
         console.log(modalProduct.productId);
         // ! axios delete
         console.log("axios delete")
         axios
             .delete("http://localhost:8080/api/product/remove",
-            {
-                data: {
-                    productId : modalProduct.productId
+                {
+                    data: {
+                        productId: modalProduct.productId
+                    }
                 }
-            }
             )
             .then(() => {
                 alert("상품 삭제 완료");
@@ -57,7 +73,7 @@ function AllProductManagement(props) {
     }
 
     function MyVerticallyCenteredModal(props) {
-        console.log("모달 : " + JSON.stringify(modalProduct));
+        // console.log("모달 : " + JSON.stringify(modalProduct));
         return (
             <>
                 {modalProduct !== undefined ?
@@ -76,7 +92,7 @@ function AllProductManagement(props) {
                         </Modal.Body>
                         <ModalFooter>
                             <div style={{ textAlign: "center" }}>
-                                <Link to="../updateProduct" state={{data: modalProduct}}><Button variant="success"> 수정 </Button></Link>
+                                <Link to="../updateProduct" state={{ data: modalProduct }}><Button variant="success"> 수정 </Button></Link>
                                 <Button variant="danger" onClick={deleteProduct}> 삭제 </Button>
                             </div>
                         </ModalFooter>
@@ -86,6 +102,74 @@ function AllProductManagement(props) {
 
         );
     }
+
+    
+
+    function plus() {
+
+        if ((firstPage + 5) <= totalPage){
+            setFirstPage(firstPage + 5);
+            setPage(firstPage + 5)
+            
+        } 
+        if ((lastPage + 5) <= totalPage) setLastPage(lastPage + 5);
+        else setLastPage(totalPage);
+        changePagination();
+        
+    }
+    function minus() {
+        if ((firstPage - 5) >= 1) setFirstPage(firstPage - 5);
+        if ((firstPage == 1)) setFirstPage(1);
+        if ((lastPage - 5) >= 5){
+            setLastPage(lastPage - 5);
+            setPage(lastPage - 5)
+        } 
+
+        if (lastPage == totalPage){
+            setLastPage(firstPage - 1);
+            setPage(lastPage - 1)
+        } 
+
+        console.error("firstPage = " + firstPage);
+        changePagination();
+
+    }
+
+    if (totalPage !== undefined) {
+        for (let number = 1; number <= totalPage; number++) {
+            items.push(
+                <Pagination.Item key={number} active={number === page} onClick={(data) => test(number)}>
+                    {number}
+                </Pagination.Item>
+            );
+        }
+        changePagination();
+    }
+
+
+    function changePagination() {
+        showItems = [];
+        console.log("firstPage = " + firstPage);
+        console.log("lastPage = " + lastPage);
+        for (let number = firstPage; number <= lastPage; number++) {
+            showItems.push(
+                items[number - 1]
+            );
+        }
+        console.warn();
+
+        // console.log("Items = " + items);
+        // console.log("showItems = " + showItems);
+
+    }
+
+    function test(number) {
+        console.log(number + " 눌렀따");
+        setPage(number)
+    };
+
+
+
     return (
         <div>
             전체 상품 관리
@@ -133,14 +217,27 @@ function AllProductManagement(props) {
             <div>
                 <InputGroup className="mb-3">
                     <FormControl
-                        placeholder="Recipient's username"
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
+                        placeholder=""
+                        onChange={(e)=>setWord(e.target.value)}
+                        // aria-label="Recipient's username"
+                        // aria-describedby="basic-addon2"
                     />
-                    <Button variant="outline-secondary" id="button-addon2">
+                    <Button variant="outline-secondary" id="button-addon2" onClick={()=>setCheckWord(!checkWord)}>
                         Button
                     </Button>
                 </InputGroup>
+                <div>
+                    <Pagination>
+                        {/* <Pagination.First /> */}
+                        <Pagination.Prev onClick={minus} />
+                        {
+                            showItems
+                        }
+                        <Pagination.Next onClick={plus} />
+                        {/* <Pagination.Last /> */}
+                    </Pagination>
+                    <br />
+                </div>
                 <Link to={"/registrationProduct"}><Button variant='warning'>등록</Button></Link>
             </div>
         </div>
