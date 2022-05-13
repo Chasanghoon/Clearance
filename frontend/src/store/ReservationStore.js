50a3aefa8267d85f0df3af0307547be03de8f1f1
rkdrhkddms <rhkddms3501@naver.com>
2022-05-14 02:34:32 +0900

    [S06P31E203-210] Feat : 마이페이지_유저 전체 예약 내역_전체예약리스트

--- frontend/src/store/ReservationStore.js
+++ frontend/src/store/ReservationStore.js
@@ -11,6 +11,7 @@ const ReservationStore = create((set) => ({
     
     storeId: "",
     setStoreId: (e) => set({ storeId: e }),
+    
     bookSet: "",
     setBookSet: (e) => set({ bookSet: e }),
 
